b08681280ed5fd393fe73bab0c298869470183c7
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2023-07-05 14:11:39 +0300

    Update test.tsx

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,36 +1,36 @@
-// import { Button } from "@chakra-ui/react";
-// import { Media } from "@prisma/client";
-// import React, { useEffect, useState } from "react";
-// import { Controller, useForm } from "react-hook-form";
-// import ManageProductMedia from "~/components/ManageMedia";
+import { Button } from "@chakra-ui/react";
+import { Media } from "@prisma/client";
+import React, { useEffect, useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import ManageProductMedia from "~/components/ManageMedia";
 
-// interface MediaForm {
-//   media: Media[];
-// }
+interface MediaForm {
+  media: Media[];
+}
 
-// function Test() {
-//   useEffect(() => {
-//     if (window) {
-//       const moyasar = window.Moyasar;
+function Test() {
+  useEffect(() => {
+    if (window) {
+      const moyasar = window.Moyasar;
 
-//       moyasar.init({
-//         element: "#my-form",
-//         amount: 1000,
-//         currency: "SAR",
-//         description: "Coffee Order #1",
-//         publishable_api_key: "pk_test_AQpxBV31a29qhkhUYFYUFjhwllaDVrxSq5ydVNui",
-//         callback_url: "https://moyasar.com/thanks",
-//         methods: ["creditcard"],
-//       });
-//     }
-//   }, []);
+      moyasar.init({
+        element: "#my-form",
+        amount: 1000,
+        currency: "SAR",
+        description: "Coffee Order #1",
+        publishable_api_key: "pk_test_AQpxBV31a29qhkhUYFYUFjhwllaDVrxSq5ydVNui",
+        callback_url: "https://moyasar.com/thanks",
+        methods: ["creditcard"],
+      });
+    }
+  }, []);
 
-//   return (
-//     <>
-//     <Head></Head>
-//       <div id={"my-form"}></div>
-//     </>
-//   );
-// }
+  return (
+    <>
+    <Head></Head>
+      <div id={"my-form"}></div>
+    </>
+  );
+}
 
-// export default Test;
+export default Test;
