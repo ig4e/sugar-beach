149e005ad835a8091b21b663dcecf5d171e2a3c4
Ahmed Mohamed <50212311+ig4e@users.noreply.github.com>
2023-07-11 04:53:43 +0300

    [chore] removed useDrawerPlacement

--- src/hooks/useDrawerPlacment.tsx
+++
@@ -1,9 +0,0 @@
-import { useLocale } from "next-intl";
-import type { Locale } from "~/types/locale";
-
-function useDrawerPlacment() {
-  const locale = useLocale() as Locale;
-  return locale === "en" ? "right" : ("left" as const);
-}
-
-export default useDrawerPlacment;
